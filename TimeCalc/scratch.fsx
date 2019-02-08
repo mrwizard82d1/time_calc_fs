fdf82af2025d2ce7356005e16b9094ad0efcfa6c
Larry Jones <larry.jones@reveal-energy.com>
2019-02-08 03:11:12 -0600

    Prototype partitionBy function (at least for Lists)

--- TimeCalc/scratch.fsx
+++ TimeCalc/scratch.fsx
@@ -73,3 +73,21 @@ let parsedLines = [
           ParsedDate {DayOfMonth=DayOfMonth 5; Month=Some Feb};
           ParsedActivityStart{Hour=HourOfDay 22; Minute= MinuteOfHour 55; Details=Details "baz"};
           ]
+
+let rec partitionBy f l =
+    match l with
+    | [] -> []
+    | x::xs ->
+        let first = f x
+        let run = List.takeWhile (fun y -> f y = first) l
+        run::(partitionBy f (List.skip (List.length run) l))
+    
+printfn "partitionBy id [] = %A" (partitionBy id [])
+printfn "partitionBy id [ 1 ] = %A" (partitionBy id [ 1 ])
+let isOdd x = (x % 2 = 0)
+printfn "partitionBy isOdd [] = %A" (partitionBy isOdd [])
+printfn "partitionBy isOdd [ 1 ] = %A" (partitionBy isOdd [ 1 ])
+printfn "partitionBy isOdd [ 1 2 ] = %A" (partitionBy isOdd [ 1; 2 ])
+let isPrime x =
+    List.contains x [2; 3; 5; 7; 11; 13; 17]
+printfn "partitionBy isPrime [2 .. 17] = %A" (partitionBy isPrime [2..17])
